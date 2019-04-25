f5f2a073a7c0420dfaa1f2f0fc3e0b79cc5b4c30
JG318682 <37377626+JG318682@users.noreply.github.com>
2019-04-25 10:32:29 -0500

    Super Hard work in Progress

---
+++ src/startUpDisplay.java
@@ -0,0 +1,13 @@
+public class startUpDisplay {
+
+    private static void welcome() {
+        //System.out.println("Welcome to BTD()");
+        //System.out.println("Choose your difficulty");
+        //System.out.println("Type 1 for Easy, 2 for Medium, 3 for hard");
+
+
+
+    }
+
+
+}
